5307e2e8bb9dff82d1e7069eaa566cd4dc7a950d
LinlyBoi <libkyy@e.email>
2023-03-22 08:06:34 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    let state = [[0u8; 4]; 4];
+}
+#[derive(Debug)]
+struct Puzzle {}
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}

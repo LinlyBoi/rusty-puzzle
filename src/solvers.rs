c00f80f87b1736352f7e339b78681e7955ed5f83
LinlyBoi <libkyy@e.email>
2023-03-26 20:46:23 +0200

    fixed solution infinite loop (idk how but i did)

--- src/solvers.rs
+++ src/solvers.rs
@@ -1,61 +1,81 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
-use crate::Puzzle;
+use crate::{find_index, Puzzle};
 
+#[derive(Debug)]
 pub struct Solution {
     goal_path: Vec<Puzzle>,
     cost: usize,
     explored: HashSet<Puzzle>,
 }
 impl Solution {
-    fn from(&mut self, goal: Puzzle, explored: HashSet<Puzzle>) -> Self {
+    pub fn from_goal(goal: Puzzle, explored: HashSet<Puzzle>, cost: usize) -> Solution {
         let mut path: Vec<Puzzle> = vec![];
-        let mut cost: usize = 0;
-        loop {
-            match goal.clone().parent {
-                None => {
-                    self.goal_path = path.clone();
-                    self.cost = cost;
-                    self.explored = explored.clone();
-                }
-                Some(puzzle) => {
-                    path.push(*puzzle);
-                    cost += 1;
-                }
+        match goal.clone().parent {
+            None => Solution {
+                goal_path: path.clone(),
+                cost,
+                explored: explored.clone(),
+            },
+            Some(puzzle) => {
+                path.push(*puzzle);
+                Self::from_goal(goal, explored, cost + 1)
             }
-        }
-    }
-    fn new(goal: Puzzle, explored: HashSet<Puzzle>) -> Solution {
-        let mut path: Vec<Puzzle> = vec![];
-        let mut cost: usize = 0;
-        loop {
-            match goal.clone().parent {
-                None => Solution {
-                    goal_path: path.clone(),
-                    cost,
-                    explored: explored.clone(),
-                },
-                Some(puzzle) => {
-                    path.push(*puzzle);
-                    cost += 1;
-                    continue;
-                }
-            };
         }
     }
     pub fn get_path(self) -> Vec<Puzzle> {
         self.goal_path
     }
 }
-pub fn solve_dfs(mut frontier: Vec<Puzzle>, mut explored: HashSet<Puzzle>) -> Option<Solution> {
-    match frontier.pop() {
+pub fn solve_dfs(
+    mut frontier: VecDeque<Puzzle>,
+    mut explored: HashSet<Puzzle>,
+) -> Option<Solution> {
+    match frontier.pop_front() {
         None => None,
         Some(node) => {
             explored.insert(node.clone());
             match node.clone().checkgoal() {
-                true => Some(Solution::new(node.clone(), explored)),
+                true => Some(Solution::from_goal(node.clone(), explored, 0)),
                 false => {
-                    let children = node.clone().getchildren();
+                    let parent = node.clone().getchildren();
+                    for child in parent.neighbours {
+                        let mut dup = false;
+                        for node in explored.clone() {
+                            if child.clone().equals(node) {
+                                dup = true;
+                                break;
+                            }
+                        }
+                        for node in frontier.clone() {
+                            if child.clone().equals(node) {
+                                dup = true;
+                                break;
+                            }
+                        }
+                        if !dup {
+                            frontier.push_front(child)
+                        }
+                    }
+                    dbg!(node.state, frontier.clone().len(), explored.clone().len());
+                    solve_dfs(frontier, explored)
+                }
+            }
+        }
+    }
+}
+pub fn solve_bfs(
+    mut frontier: VecDeque<Puzzle>,
+    mut explored: HashSet<Puzzle>,
+) -> Option<Solution> {
+    match frontier.pop_front() {
+        None => None,
+        Some(node) => {
+            explored.insert(node.clone());
+            match node.clone().checkgoal() {
+                true => Some(Solution::from_goal(node.clone(), explored, 0)),
+                false => {
+                    let children = node.clone().getchildren().neighbours;
                     for child in children {
                         let mut dup = false;
                         for node in explored.clone() {
@@ -71,19 +91,22 @@ pub fn solve_dfs(mut frontier: Vec<Puzzle>, mut explored: HashSet<Puzzle>) -> Op
                             }
                         }
                         if !dup {
-                            frontier.push(child)
+                            frontier.push_front(node.clone());
                         }
-
-                        println!(
-                            "Frontier lenght: {} \n explored length: {} \n funny state: {:#?}",
-                            frontier.clone().len(),
-                            explored.clone().len(),
-                            node.clone().state
-                        );
                     }
-                    solve_dfs(frontier, explored)
+                    dbg!(node.state, frontier.clone().len(), explored.clone().len());
+                    solve_bfs(frontier, explored)
                 }
             }
         }
     }
+}
+pub fn mann_heust((curr_x, curr_y): (usize, usize), value: u8, goal: Puzzle) -> usize {
+    let (goal_x, goal_y) = find_index(goal.state, value).expect("not exist");
+    curr_x.abs_diff(goal_x) + curr_y.abs_diff(goal_y)
+}
+pub fn eucl_heust((curr_x, curr_y): (usize, usize), value: u8, goal: Puzzle) -> usize {
+    let (goal_x, goal_y) = find_index(goal.state, value).expect("not exist");
+    let square = curr_x.pow(2).abs_diff(goal_x).pow(2) + curr_y.pow(2).abs_diff(goal_y).pow(2);
+    (square as f64).sqrt() as usize
 }

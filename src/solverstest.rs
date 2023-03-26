6f4bf915f56ec63c3b7aa9c13213e7556677c988
LinlyBoi <libkyy@e.email>
2023-03-26 20:46:02 +0200

    this still doesn't work :(

--- src/solverstest.rs
+++ src/solverstest.rs
@@ -1,9 +1,11 @@
+use std::collections::HashSet;
+
 use crate::{init_puz, solvers::solve_dfs};
 
 // #[test]
 // fn dfs() {
 //     let rows = vec![vec![5, 2, 8], vec![4, 1, 7], vec![0, 3, 6]];
-//     let test_puzzle = init_puz(rows, (0, 2));
-//     let solly = solve_dfs(vec![test_puzzle], vec![]).expect("Nope");
+//     let test_puzzle = init_puz(rows);
+//     let solly = solve_dfs(vec![test_puzzle], HashSet::new()).expect("Nope");
 //     assert!(solly.get_path().pop().expect("??").checkgoal())
 // }

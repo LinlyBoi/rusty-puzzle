d8ef40d3e4b89a97d4fac5d968aeeb08163d77a9
LinlyBoi <libkyy@e.email>
2023-03-28 13:44:38 +0200

    test cases for both dfs and bfs (dfs takes a minute)

--- src/solverstest.rs
+++ src/solverstest.rs
@@ -1,11 +1,26 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
-use crate::{init_puz, solvers::solve_dfs};
+use crate::{
+    init_puz,
+    solvers::{solve_bfs, solve_dfs},
+    Puzzle,
+};
 
-// #[test]
-// fn dfs() {
-//     let rows = vec![vec![5, 2, 8], vec![4, 1, 7], vec![0, 3, 6]];
-//     let test_puzzle = init_puz(rows);
-//     let solly = solve_dfs(vec![test_puzzle], HashSet::new()).expect("Nope");
-//     assert!(solly.get_path().pop().expect("??").checkgoal())
-// }
+#[test]
+fn dfs() {
+    let rows = vec![vec![1, 4, 2], vec![3, 5, 8], vec![6, 7, 0]];
+    let test_puzzle = init_puz(rows);
+    let mut vec_q: VecDeque<Puzzle> = VecDeque::new();
+    vec_q.push_back(test_puzzle.clone());
+    let solly = solve_dfs(vec_q, HashSet::new()).expect("Nope");
+    assert!(solly.get_path().first().expect("??").clone().checkgoal())
+}
+#[test]
+fn bfs() {
+    let rows = vec![vec![1, 4, 2], vec![3, 5, 8], vec![6, 7, 0]];
+    let test_puzzle = init_puz(rows);
+    let mut vec_q: VecDeque<Puzzle> = VecDeque::new();
+    vec_q.push_back(test_puzzle.clone());
+    let solly = solve_bfs(vec_q, HashSet::new()).expect("Nope");
+    assert!(solly.get_path().first().expect("??").clone().checkgoal())
+}
